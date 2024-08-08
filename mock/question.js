3e5af696dc0ac7e811be81038dedcf1a3d95d0cf
huangjinyuan <156567947@qq.com>
2024-08-08 18:43:08 +0800

    11-2

--- mock/question.js
+++ mock/question.js
@@ -36,13 +36,14 @@ module.exports = [
     url: "/api/question",
     method: "get",
     response(ctx) {
-      const { url = "" } = ctx;
+      const { url = "", query = {} } = ctx;
       const isDeleted = url.includes("isDeleted=true");
       const isStar = url.includes("isStar=true");
+      const pageSize = parseInt(query.pageSize) || 10;
       return {
         errno: 0,
         data: {
-          list: getQuestionList({ isDeleted, isStar }),
+          list: getQuestionList({ isDeleted, isStar, LEN: pageSize }),
           total: 100,
         },
       };
